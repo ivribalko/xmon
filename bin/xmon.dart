2b976666f5369a26cabea87a1d0a04712731f585
Ivan Rybalko <ivribalko@gmail.com>
2021-03-29 07:27:22 -0400

    .

---
+++ bin/xmon.dart
@@ -0,0 +1,5 @@
+import 'package:xmon/xmon.dart' as xmon;
+
+void main(List<String> arguments) {
+  print('Hello world: ${xmon.calculate()}!');
+}
---
+++ test/xmon_test.dart
@@ -0,0 +1,8 @@
+import 'package:xmon/xmon.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
